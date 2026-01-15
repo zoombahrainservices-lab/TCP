ea02e67ed4577c70c98cad7865b07822b1eaffb4
Farzeen <farzeen@example.com>
2026-01-15 13:54:35 +0300

    Fix hydration mismatch in Input component - use React useId instead of Math.random

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useId } from 'react'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string
@@ -14,7 +16,8 @@ export default function Input({
   id,
   ...props
 }: InputProps) {
-  const inputId = id || `input-${Math.random().toString(36).substr(2, 9)}`
+  const generatedId = useId()
+  const inputId = id || generatedId
   
   return (
     <div className="w-full">
